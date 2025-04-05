8b09e327e23a0b2d626e171a8abd265111f885c2
Tim Randall <trtimrandall@gmail.com>
2025-04-05 17:05:09 +1100

    two done

--- EncryptDecrypt.py
+++ EncryptDecrypt.py
@@ -6,6 +6,7 @@ asciiPrintable = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',
                   'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
                   'y', 'z', '{', '|', '}', '~']
 
+
 def caesarEncrypt(words):
     resultWord = ""
     for char in words:
@@ -23,6 +24,7 @@ def caesarEncrypt(words):
                     resultWord += asciiPrintable[newIndex]
     print(resultWord)
 
+
 def caesarDecrypt(words):
     resultWord = ""
     for char in words:
@@ -39,7 +41,78 @@ def caesarDecrypt(words):
                 else:
                     resultWord += asciiPrintable[newIndex]
     print(resultWord)
-            
+
+
+def vigenereEncrypt(words, password):
+    resultWord = ""
+    passwordNumbers = []
+    lengthOfWords = len(words)
+    passwordNumbersForWordLength = []
+    finalIndex = 0
+    for char in password: 
+        for char2 in asciiPrintable:
+            if char == char2:
+                passwordNumbers.append(asciiPrintable.index(char2))
+
+    while lengthOfWords > 0:
+        for item in passwordNumbers:
+            passwordNumbersForWordLength.append(item)
+            lengthOfWords -= 1
+            if lengthOfWords < 1:
+                break
+        
+    for char3 in words:
+        for char2 in asciiPrintable:
+            if char3 == char2:
+                finalIndex = asciiPrintable.index(char2) + passwordNumbersForWordLength[words.index(char3)]
+                if finalIndex > len(asciiPrintable):
+                    finalIndex = finalIndex - len(asciiPrintable)
+                    resultWord += asciiPrintable[finalIndex]
+                elif finalIndex < 0:
+                    finalIndex = len(asciiPrintable) + finalIndex
+                    resultWord += asciiPrintable[finalIndex]
+                else:
+                    resultWord += asciiPrintable[finalIndex]
+
+    print(resultWord)
+
+def vigenereDecrypt(words, password):
+    resultWord = ""
+    passwordNumbers = []
+    lengthOfWords = len(words)
+    passwordNumbersForWordLength = []
+    finalIndex = 0
+    for char in password: 
+        for char2 in asciiPrintable:
+            if char == char2:
+                passwordNumbers.append(asciiPrintable.index(char2))
+
+    while lengthOfWords > 0:
+        for item in passwordNumbers:
+            passwordNumbersForWordLength.append(item)
+            lengthOfWords -= 1
+            if lengthOfWords < 1:
+                break
+        
+    for char3 in words:
+        for char2 in asciiPrintable:
+            if char3 == char2:
+                finalIndex = asciiPrintable.index(char2) - passwordNumbersForWordLength[words.index(char3)]
+                if finalIndex > len(asciiPrintable):
+                    finalIndex = finalIndex - len(asciiPrintable)
+                    resultWord += asciiPrintable[finalIndex]
+                elif finalIndex < 0:
+                    finalIndex = len(asciiPrintable) + finalIndex
+                    resultWord += asciiPrintable[finalIndex]
+                else:
+                    resultWord += asciiPrintable[finalIndex]
+
+    print(resultWord)
+    
 caesarEncrypt("alking about the history and current state of death metal and re~")
 
 caesarDecrypt("donlqj#derxw#wkh#klvwru|#dqg#fxuuhqw#vwdwh#ri#ghdwk#phwdo#dqg#uh")
+
+vigenereEncrypt("my brain is melting", "catbus")
+
+vigenereDecrypt("Q[tEhUMPtMitQGaWMPK", "catbus")
